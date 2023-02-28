d1aca0029bbef16c0fc1f8032cf1d36b3664c1f4
Fran Cili <fcili825@gmail.com>
2023-02-28 10:39:39 +0100

    added url routes

--- beer_app/main/urls.py
+++ beer_app/main/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import BeerViewSet, ManufacturerViewSet
+from .views import BeerViewSet, ManufacturerViewSet, BeerTypeViewSet, ReviewViewSet
 
 router = DefaultRouter()
 router.register(r'beers', BeerViewSet, basename='beer')
 router.register(r'manufacturers', ManufacturerViewSet, basename='manufacturer')
-
+router.register(r'beer types', BeerTypeViewSet, basename='beer type')
+router.register(r'reviews', ReviewViewSet, basename='review')
 urlpatterns = router.urls
